7006460a2862c6378b38db96e038179ed1be364f
freelancemessiaen <153985208+freelancemessiaen@users.noreply.github.com>
2024-04-09 15:36:31 +0200

    mise en place des images
    fin du projet avec la mise en place des images

--- App.js
+++ App.js
@@ -1,15 +1,43 @@
 import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { s } from "./App.style";
+import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
 
 export default function App() {
+  const [imageURIList, setImageURIList] = useState([]);
+  async function pickImageAsync() {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+    if (result.canceled) {
+      alert("pas d'image chargé");
+    } else {
+      setImageURIList([...imageURIList, result.assets[0].uri]);
+    }
+  }
   return (
     <SafeAreaProvider>
       <SafeAreaView style={{ flex: 1 }}>
         <Text style={s.title}>Mes photos préférées</Text>
-        <View style={s.body}></View>
+        <View style={s.body}>
+          <ScrollView>
+            {imageURIList.map((imageURI, i) => (
+              <Image
+                style={{
+                  height: 200,
+                  marginVertical: 30,
+                  width: "100%",
+                }}
+                key={imageURI + i}
+                source={{ uri: imageURI }}
+              />
+            ))}
+          </ScrollView>
+        </View>
         <View style={s.footer}>
-          <TouchableOpacity style={s.btn} onPress={() => ""}>
+          <TouchableOpacity style={s.btn} onPress={pickImageAsync}>
             <Text style={s.btnTxt}>Ajouter photo</Text>
           </TouchableOpacity>
         </View>
